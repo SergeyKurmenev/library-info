8ef93b674c187cfccc249d5c1c802430df820a5f
skurm <sergeykurmenev@gmail.com>
2022-09-22 12:45:15 +0400

    init

---
+++ src/test/java/com/example/libraryinfo/LibraryInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
